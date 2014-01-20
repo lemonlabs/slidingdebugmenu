d231d46c53c6962c339349533ba7a44b47da685e
Balys Valentukevicius <balys.v@gmail.com>
2014-01-20 14:06:38 +0200

    Added more examples in demo

--- demo/src/main/java/co/lemonlabs/android/slidingdebugmenu/demo/DemoActivity.java
+++ demo/src/main/java/co/lemonlabs/android/slidingdebugmenu/demo/DemoActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 import co.lemonlabs.android.slidingdebugmenu.SlidingDebugMenu;
+import co.lemonlabs.android.slidingdebugmenu.modules.LocationModule;
 
 public class DemoActivity extends ActionBarActivity {
 
@@ -13,8 +14,22 @@ public class DemoActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demo);
+
+        // Remove module using editor
+        SlidingDebugMenu.edit()
+                .remove(LocationModule.class)
+                .commit();
+
+        // attach drawer
         menu = SlidingDebugMenu.attach(this);
 
+        // ... some code
+
+        // re-add module at position 1
+        menu.addModule(1, new LocationModule(), false);
+
+        // show/hide the settings at the bottom of the drawer
+        menu.setDrawerSettingsVisible(true);
     }
 
     @Override
